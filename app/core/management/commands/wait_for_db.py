13ce940e0edcc630fb61cc0b649811a4a6f3d31f
Anderson <alvesmonteiroanderson@gmail.com>
2024-05-26 13:40:24 -0300

    Dockergignore wait_for_db mock command

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -3,3 +3,8 @@ Django command to wait until database is available.
 """
 from django.core.management.base import BaseCommand
 
+
+class Command(BaseCommand):
+    """Django command to wait for database to be available."""
+    def handle(self, *args, **options):
+        pass
