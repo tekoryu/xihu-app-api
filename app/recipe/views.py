0376b3f30fac75247633a9a7f7e6ec4813f8a33e
tekoryu <anderson.monteiro@presidencia.gov.br>
2024-06-10 17:57:23 -0300

    0.0.7 Tests for Tag API

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,11 +1,18 @@
 """
 Views for the Recipe API
 """
-from rest_framework import viewsets
+from rest_framework import (
+    viewsets,
+    mixins,
+)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
+
 from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -29,3 +36,19 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+class TagViewSet(
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+    ):
+    """View for manage tag APIs"""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get_queryset(self):
+        """Filter recipes by user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
